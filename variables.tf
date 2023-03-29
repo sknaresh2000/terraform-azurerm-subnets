73edaa98417b871ba6493e89dfb8f46a036d304a
NareshKumaran Sathiaseelan <naresh@nareshkransmini.mynetworksettings.com>
2023-03-29 13:48:42 -0400

    Fixed NSG rules type

--- variables.tf
+++ variables.tf
@@ -68,8 +68,8 @@ variable "nsgrules" {
     protocol                     = string
     source_port_range            = string
     destination_port_range       = string
-    source_port_ranges           = string
-    destination_port_ranges      = string
+    source_port_ranges           = list(string)
+    destination_port_ranges      = list(string)
     source_address_prefix        = string
     destination_address_prefix   = string
     source_address_prefixes      = list(string)
